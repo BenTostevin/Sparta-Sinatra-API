5d3bf19e9a09853f5d3facdde14da54f67057f94
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-21 11:42:27 +0100

    Added test data

--- controllers/countries_controller.rb
+++ controllers/countries_controller.rb
@@ -8,35 +8,55 @@ class CountriesController < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # Index
-  get '/countries' do
-    'index'
-  end
+  $dummy_data = [ {
+    :id => 0,
+    :country_name => 'UK',
+    :continent => 'Europe',
+    :population => 70,
+    :language => 'English',
+    :world_cup => true
+  },
+  {
+    :id => 1,
+    :country_name => 'Australia',
+    :continent => 'Oceanic',
+    :population => 30,
+    :language => 'English',
+    :world_cup => false
+  }
+  ]
+
+# Index
+get '/countries' do
+  @countries = $dummy_data
+
+  erb :'countries/index'
+end
 
 
-  get "/countries/new" do
-    'new'
-  end
+get "/countries/new" do
+  'new'
+end
 
-  get "/countries/:id" do
-    "show "
-  end
+get "/countries/:id" do
+  "show "
+end
 
-  post '/countries' do
-    'create'
-  end
+post '/countries' do
+  'create'
+end
 
-  get '/countries/:id/edit' do
-    "edit"
-  end
+get '/countries/:id/edit' do
+  "edit"
+end
 
-  put '/countries/:id' do
-    "update"
-  end
+put '/countries/:id' do
+  "update"
+end
 
-  delete '/countries/:id' do
-    "destroy"
-  end
+delete '/countries/:id' do
+  "destroy"
+end
 
 
 
